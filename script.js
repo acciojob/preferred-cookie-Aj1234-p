df5e6d326bc1a1a4b1fbc9818367963901e7c6f9
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-09-18 13:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let form = document.querySelector('form');
 let submitButton = document.querySelector('#btn');
 let fetchUserFontSize = document.querySelector('#fontsize');
 let fetchUserFontColor = document.querySelector('#fontcolor');
-
 function fetchCookieFromStorage(){
   if(!document.cookie) {
-    console.log("Still this time, cookies are not stored");
+    console.log("Still this time, cookies are not stored") 
     return;
   }
   let cookies = document.cookie.split(';'),color, size;
@@ -15,14 +14,24 @@ function fetchCookieFromStorage(){
     if(key.trim().toLowerCase()==='fontcolor'.toLowerCase()) color = value.trim();
     if(key.trim().toLowerCase()==='fontsize'.toLowerCase()) size = value.trim();
   }
- if(color) fetchUserFontColor.value = color;
-  if(size) fetchUserFontSize.value = size;
+  if(color) {
+     fetchUserFontColor.value = color;
+    document.documentElement.style.setProperty('--font-color',color);
+  }
+  if(size){
+     fetchUserFontSize.value = size; 
+     document.documentElement.style.setProperty('--font-size',`${size}px`);
+  }
 }
 
 submitButton.addEventListener('click',(e)=>{
   e.preventDefault();
-  document.cookie = `fontcolor=${fetchUserFontColor.value}; max-age=${60*60*24*30}; path=/`;
-  document.cookie = `fontsize=${fetchUserFontSize.value}; max-age=${60*60*24*30}; path=/`;
+  const color = fetchUserFontColor.value;
+  const size = fetchUserFontSize.value;
+  document.cookie = `fontcolor=${color}; max-age=${60*60*24*30}; path=/`;
+  document.cookie = `fontsize=${size}; max-age=${60*60*24*30}; path=/`;
+  document.documentElement.style.setProperty('--font-color',color);
+  document.documentElement.style.setProperty('--font-size',`${size}px`);
 })
 
 fetchCookieFromStorage();
